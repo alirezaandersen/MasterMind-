3f6ed38fda8d5b01bd0e6c4a4c88f1d0ab507e24
Alireza Andersen <alireza.andersen@gmail.com>
2016-01-28 20:36:35 -0700

    updated constants

--- constants.rb
+++ constants.rb
@@ -1,6 +1,5 @@
 require_relative 'settings'
 
-
 module Constants
 
   include Settings
@@ -9,4 +8,5 @@ module Constants
 
   INPUT_TOO_SHORT = "If it's fewer than #{NUMBER_OF_LETTERS} letters, tell them it's too short"
   INPUT_TOO_LONG = "If it's longer than #{NUMBER_OF_LETTERS} letters, tell them it's too long"
+  INVALID_LETTER_USED = "Not a valid letter"
 end
