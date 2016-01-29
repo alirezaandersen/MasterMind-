f7d22591a23ddc5a01bd79eb27947749170f3d4b
Alireza Andersen <alireza.andersen@gmail.com>
2016-01-28 20:35:59 -0700

    have loop for game

--- game_flow.rb
+++ game_flow.rb
@@ -8,8 +8,11 @@ class GameFlow
   include Constants
   include Settings
 
+  attr_accessor :counter
+
   def initialize
     @sequence = []
+    @counter = 0
     randomizer
   end
 
@@ -22,9 +25,16 @@ class GameFlow
   def play
     puts "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game."
     puts "What's your guess?"
-    print ">"
-    input = gets.chomp
-    game_input(input)
+    in_game
+  end
+
+  def in_game
+    loop do
+      print ">"
+      input = gets.chomp
+      game_input(input)
+      break if input == @sequence.join
+    end
   end
 
   def game_input(input)
@@ -33,28 +43,53 @@ class GameFlow
       puts INPUT_TOO_LONG
     elsif input.length < NUMBER_OF_LETTERS
       puts INPUT_TOO_SHORT
+    elsif !validate(input)
+      puts INVALID_LETTER_USED
     else
-      puts "good guess."
+      process(input)
     end
-    #process input
-    process(input)
+    # process input
+    # process(input)
+  end
+
+  def validate(input)
+    input.chars.each do |char|
+      return false if !VALID_LETTERS.include?(char)
+    end
+    return true
   end
 
   def process(input)
-    #convert input to array
-    input_chars = input.chars
-    #compare input to sequence
+    # convert input to array
+    letters = correct_num_of_letters(input)
+    positions = correct_positions(input)
+    #need to create a quit
+    #need a cheat method
 
+    puts "#{input} has #{letters} of the correct elements with #{positions} in the correct positions. You've taken 1 guess"
+  end
+
+  def correct_positions(input)
+    input_chars = input.chars
     x = input_chars.zip(@sequence).map { |a, b| a == b }
     num_correct_positions = x.count(true)
+  end
 
+  # def correct_num_of_letters2(input)
+    input_chars = input.chars
     num_correct_letters_hash = @sequence.uniq.map do |char|
       input_chars.frequency.fetch(char,0)
     end
     num_correct_letters = num_correct_letters_hash.reduce(:+)
+  end
 
-    #print something to the string
-    require 'pry' ; binding.pry
+  def correct_num_of_letters(input)
+    num_correct_letters =
+    sum = 0
+    input.chars.uniq.each{ |char|
+      sum += @sequence.count(char)
+    }
+    sum
   end
 
   def instructions
